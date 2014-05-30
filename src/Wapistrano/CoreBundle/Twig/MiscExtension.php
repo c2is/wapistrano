4fe514ace98a2f8bd5fa681adcbda4569fb5c027
a.cianfarani <a.cianfarani@c2is.fr>
2014-05-30 09:05:03 +0200

    Added a twig ext to handle ajax loader display

---
+++ src/Wapistrano/CoreBundle/Twig/MiscExtension.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Wapistrano\CoreBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
+
+class MiscExtension extends \Twig_Extension
+{
+    protected $container;
+
+    /**
+     * Constructor.
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Returns a list of global functions to add to the existing list.
+     *
+     * @return array An array of global functions
+     */
+    public function getFunctions()
+    {
+        return array(
+            'wapi_render_loader' => new \Twig_Function_Method($this, 'renderLoader', array('is_safe' => array('html')))
+        );
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'wapi_misc';
+    }
+
+    public function renderLoader($parameters = array(), $name = null)
+    {
+
+        return $this->container->get("templating")->render("WapistranoCoreBundle:Misc:loader.html.twig",
+            array("message" => $parameters["message"]));
+
+    }
+
+}
