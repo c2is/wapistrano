71c6c53d831929aa865fc863608f663d87a58347
a.cianfarani <a.cianfarani@c2is.fr>
2014-06-26 06:59:44 +0200

    Clean code

--- src/Wapistrano/CoreBundle/DataFixtures/ORM/LoadDatasourceData.php
+++ src/Wapistrano/CoreBundle/DataFixtures/ORM/LoadDatasourceData.php
@@ -4,6 +4,10 @@ namespace Wapistrano\CoreBundle;
 use \Doctrine\Common\DataFixtures\FixtureInterface,
     \Doctrine\Common\Persistence\ObjectManager;
 use Wapistrano\CoreBundle\entity\Users;
+use Wapistrano\CoreBundle\entity\Projects;
+use Wapistrano\CoreBundle\entity\Hosts;
+use Wapistrano\CoreBundle\entity\Recipes;
+use Wapistrano\CoreBundle\entity\Stages;
 
 /**
  * Class LoadDatasourceData
@@ -21,11 +25,36 @@ class LoadDatasourceData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
+        $today = new \DateTime();
+
+        // Users
         $user = new Users();
         $user->setLogin("admin");
         $user->setCryptedPassword("admin");
         $user->setAdmin(1);
         $manager->persist($user);
+        $manager->flush();
+
+        $user = new Users();
+        $user->setLogin("dev");
+        $user->setCryptedPassword("dev");
+        $user->setAdmin(0);
+        $manager->persist($user);
+        $manager->flush();
+
+        // Project
+        $project = new Projects();
+        $project->setName("Project One");
+        $project->setDescription("Project One Description");
+        $project->setCreatedAt($today);
+        $manager->persist($project);
+        $manager->flush();
+
+        $project = new Projects();
+        $project->setName("Project Two");
+        $project->setDescription("Project Two Description");
+        $project->setCreatedAt($today);
+        $manager->persist($project);
         $manager->flush();
     }
 
